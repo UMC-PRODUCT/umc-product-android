09a2633313ca21495df50218c1464bdf0a6b863f
seok <rudtjr1206@naver.com>
2026-01-10 19:06:50 +0900

    [feat/#9] - 인증 실패 ui 추가

---
+++ presentation/src/main/java/com/umc/presentation/ui/signUp/fail/SignUpFailFragment.kt
@@ -0,0 +1,47 @@
+package com.umc.presentation.ui.signUp.fail
+
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.umc.presentation.base.BaseFragment
+import com.umc.presentation.base.UiState
+import com.umc.presentation.databinding.FragmentSignUpFailBinding
+import kotlinx.coroutines.launch
+
+class SignUpFailFragment : BaseFragment<FragmentSignUpFailBinding, UiState, SignUpFailEvent, SignUpFailViewModel>(
+    FragmentSignUpFailBinding::inflate,
+) {
+    override val viewModel: SignUpFailViewModel by viewModels()
+
+    override fun initView() {
+        binding.apply {
+            vm = viewModel
+        }
+    }
+
+    override fun initStates() {
+        super.initStates()
+
+        repeatOnStarted(viewLifecycleOwner) {
+            launch {
+                viewModel.uiEvent.collect {
+                    handleEvent(it)
+                }
+            }
+
+            launch {
+                viewModel.uiState.collect {
+                    // TODO 상태 관리
+                }
+            }
+        }
+    }
+
+    override fun handleEvent(event: SignUpFailEvent) {
+        when (event) {
+            SignUpFailEvent.MoveToBack -> findNavController().popBackStack()
+            SignUpFailEvent.MoveToHomePage -> {
+                //TODO 공식 홈페이지 url 이동
+            }
+        }
+    }
+}
---
+++ presentation/src/main/java/com/umc/presentation/ui/signUp/fail/SignUpFailViewModel.kt
@@ -0,0 +1,25 @@
+package com.umc.presentation.ui.signUp.fail
+
+import com.umc.presentation.base.BaseViewModel
+import com.umc.presentation.base.UiEvent
+import com.umc.presentation.base.UiState
+import javax.inject.Inject
+
+class SignUpFailViewModel
+@Inject
+constructor() : BaseViewModel<UiState, SignUpFailEvent>(
+    UiState.Default,
+) {
+    fun onClickBack() {
+        emitEvent(SignUpFailEvent.MoveToBack)
+    }
+
+    fun onClickHomePage() {
+        emitEvent(SignUpFailEvent.MoveToHomePage)
+    }
+}
+
+sealed class SignUpFailEvent : UiEvent {
+    object MoveToBack : SignUpFailEvent()
+    object MoveToHomePage : SignUpFailEvent()
+}
