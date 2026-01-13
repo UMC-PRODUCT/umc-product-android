99536788c8d63082eacebe97f7b578112ded9a9c
yangjiae12 <yangjiae12@gmail.com>
2026-01-13 13:26:00 +0900

    [feat/#18] - 출석 체크 RecyclerView 어댑터 구현

---
+++ presentation/src/main/java/com/umc/presentation/ui/act/adapter/CheckAvailableAdapter.kt
@@ -0,0 +1,58 @@
+package com.umc.presentation.ui.act.adapter
+
+import android.transition.AutoTransition
+import android.transition.TransitionManager
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.umc.presentation.databinding.ItemActCheckAvailableBinding
+import com.umc.presentation.ui.act.check.CheckAvailableUIModel
+
+class CheckAvailableAdapter(
+    private val onItemClick: (Int) -> Unit
+) : ListAdapter<CheckAvailableUIModel, CheckAvailableAdapter.ViewHolder>(
+    AvailableSessionDiffCallback()
+) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemActCheckAvailableBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    inner class ViewHolder(private val binding: ItemActCheckAvailableBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(uiModel: CheckAvailableUIModel) {
+            binding.uiModel = uiModel
+
+            binding.root.setOnClickListener {
+                val parent = itemView.parent as? ViewGroup
+                if (parent != null) {
+                    val transition = AutoTransition().apply { duration = 300 }
+                    TransitionManager.beginDelayedTransition(parent, transition)
+                }
+                onItemClick(uiModel.session.id)
+            }
+
+            binding.executePendingBindings()
+        }
+    }
+
+    class AvailableSessionDiffCallback : DiffUtil.ItemCallback<CheckAvailableUIModel>() {
+        override fun areItemsTheSame(oldItem: CheckAvailableUIModel, newItem: CheckAvailableUIModel) =
+            oldItem.session.id == newItem.session.id
+
+        override fun areContentsTheSame(oldItem: CheckAvailableUIModel, newItem: CheckAvailableUIModel) =
+            oldItem == newItem
+    }
+}
