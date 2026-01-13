c5254f858cc32f2f8217e367ca323c403634ab43
Park-yu-su <pys01022342867@gmail.com>
2026-01-14 02:23:54 +0900

    [feat/#21] - 날짜/시간 picker 구현

--- presentation/src/main/java/com/umc/presentation/ui/home/PlanAddFragment.kt
+++ presentation/src/main/java/com/umc/presentation/ui/home/PlanAddFragment.kt
@@ -1,5 +1,7 @@
 package com.umc.presentation.ui.home
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,6 +11,7 @@ import androidx.fragment.app.viewModels
 import com.umc.presentation.R
 import com.umc.presentation.base.BaseFragment
 import com.umc.presentation.databinding.FragmentPlanAddBinding
+import java.util.Calendar
 
 class PlanAddFragment : BaseFragment<FragmentPlanAddBinding, PlanAddFragmentUiState, PlanAddFragmentEvent, PlanAddViewModel>(
     FragmentPlanAddBinding::inflate,
@@ -17,12 +20,58 @@ class PlanAddFragment : BaseFragment<FragmentPlanAddBinding, PlanAddFragmentUiSt
 
     override fun initView() {
         binding.apply {
+            //onclick 달기
+            //시작 날짜/시간
+            plandetailCdvStartDate.setOnClickListener { showDatePicker(true) }
+            plandetailCdvStartTime.setOnClickListener { showTimePicker(true) }
+            //종료 날짜/시간
+            plandetailCdvEndDate.setOnClickListener { showDatePicker(false) }
+            plandetailCdvEndTime.setOnClickListener { showTimePicker(false) }
+
         }
     }
 
 
     override fun initStates() {
         super.initStates()
+        repeatOnStarted(viewLifecycleOwner){
+            viewModel.uiState.collect{ state ->
+                binding.apply {
+                    plandetailCdvStartDate.setText(state.startDateText)
+                    plandetailCdvStartTime.setText(state.startTimeText)
+                    plandetailCdvEndDate.setText(state.endDateText)
+                    plandetailCdvEndTime.setText(state.endTimeText)
+
+                }
+            }
+        }
     }
+
+    //날짜 다이얼로그 호출
+    private fun showDatePicker(isStart: Boolean) {
+        // 시작/종료 여부에 따라 현재 설정된 날짜 가져오기
+        val cal = if (isStart) viewModel.uiState.value.startDate else viewModel.uiState.value.endDate
+
+        DatePickerDialog(requireContext(), { _, year, month, day ->
+            // ViewModel 이벤트 호출 리스너 달기
+            val event = if (isStart) PlanAddFragmentEvent.UpdateStartDate(year, month, day)
+            else PlanAddFragmentEvent.UpdateEndDate(year, month, day)
+
+            viewModel.processDateTime(event)
+        },
+            cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).show()
+    }
+
+    private fun showTimePicker(isStart: Boolean) {
+        val cal = if (isStart) viewModel.uiState.value.startTime else viewModel.uiState.value.endTime
+
+        TimePickerDialog(requireContext(), { _, hour, minute ->
+            val event = if (isStart) PlanAddFragmentEvent.UpdateStartTime(hour, minute)
+            else PlanAddFragmentEvent.UpdateEndTime(hour, minute)
+            viewModel.processDateTime(event)
+        },
+            cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), false).show()
+    }
+
 
 }
