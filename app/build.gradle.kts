c58d5d6bc49e36000de631815916ba1345b161d0
yangjiae12 <yangjiae12@gmail.com>
2026-01-16 01:01:26 +0900

    [build/#24] - Naver Maps 및 Location 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,11 @@ android {
             "KAKAO_APP_KEY",
             getApiKey("kakao.native.key"),
         )
+        buildConfigField(
+            "String",
+            "NAVER_CLIENT_ID",
+            "\"${getApiKey("naver.client.id")}\""
+        )
         manifestPlaceholders["KAKAO_APP_KEY"] = getApiKey("kakao.app.key")
         applicationId = "com.umc.product"
         minSdk = 24
@@ -83,6 +88,9 @@ dependencies {
 
     // flexboxLayout
     implementation(libs.google.flexbox)
+
+    // NAVER
+    implementation("com.naver.maps:map-sdk:3.23.0")
 }
 
 fun getApiKey(propertyKey: String): String {
