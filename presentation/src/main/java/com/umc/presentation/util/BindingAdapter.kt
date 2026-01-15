c270b747fed404620d07db05995a2adf939052d0
yangjiae12 <yangjiae12@gmail.com>
2026-01-16 01:00:57 +0900

    [feat/#24] - 텍스트 선택 상태 BindingAdapter 추가

---
+++ presentation/src/main/java/com/umc/presentation/util/BindingAdapter.kt
@@ -0,0 +1,33 @@
+package com.umc.presentation.util
+
+import android.view.Gravity
+import android.view.View
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+
+object BindingAdapters {
+    @JvmStatic
+    @BindingAdapter("isSelected")
+    fun setIsSelected(view: View, isExpanded: Boolean) {
+        if (view !is TextView) return
+
+        view.isSelected = isExpanded
+
+        if (isExpanded) {
+            view.post {
+                val paint = view.paint
+                val textWidth = paint.measureText(view.text.toString())
+                val viewWidth = view.width.toFloat()
+
+                if (textWidth > viewWidth) {
+                    view.gravity = Gravity.START or Gravity.CENTER_VERTICAL
+                    view.scrollTo(0, 0)
+                } else {
+                    view.gravity = Gravity.END or Gravity.CENTER_VERTICAL
+                }
+            }
+        } else {
+            view.gravity = Gravity.END or Gravity.CENTER_VERTICAL
+        }
+    }
+}
