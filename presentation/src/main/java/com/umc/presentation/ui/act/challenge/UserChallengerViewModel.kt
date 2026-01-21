076f1a05891373bca5e59687c3ab2f6f8027edfe
yangjiae12 <yangjiae12@gmail.com>
2026-01-21 19:17:33 +0900

    [feat/#35] - 챌린저 리스트 조회 화면 로직 구현

---
+++ presentation/src/main/java/com/umc/presentation/ui/act/challenge/UserChallengerViewModel.kt
@@ -0,0 +1,73 @@
+package com.umc.presentation.ui.act.challenge
+
+import com.umc.domain.model.act.challenger.UserChallenger
+import com.umc.domain.model.enums.UserChallengerRole
+import com.umc.domain.model.enums.UserPart
+import com.umc.presentation.base.BaseViewModel
+import com.umc.presentation.base.UiEvent
+import com.umc.presentation.base.UiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class UserChallengerViewModel @Inject constructor() :
+    BaseViewModel<UserChallengerUiState, UserChallengerEvent>(UserChallengerUiState()) {
+
+    init { loadInitialData() }
+
+    private fun loadInitialData() {
+        val dummyList = listOf(
+            // PM 파트
+            UserChallenger(1, "김유엠", "유엠씨대장", 12, UserPart.PM, UserChallengerRole.LEADER),
+            UserChallenger(2, "이길동", "피엠조아", 12, UserPart.PM, UserChallengerRole.MEMBER),
+
+            // Design 파트
+            UserChallenger(3, "박디자인", "피그마마스터", 12, UserPart.DESIGN, UserChallengerRole.PART_LEADER),
+            UserChallenger(4, "최화가", "캔바장인", 12, UserPart.DESIGN, UserChallengerRole.MEMBER),
+
+            // Web 파트
+            UserChallenger(5, "정웹", "리액트왕", 12, UserPart.WEB, UserChallengerRole.PART_LEADER),
+            UserChallenger(6, "한자바", "뷰스크립트", 12, UserPart.WEB, UserChallengerRole.MEMBER),
+            UserChallenger(7, "고쿼리", "넥스트제이에스", 12, UserPart.WEB, UserChallengerRole.MEMBER),
+
+            // iOS 파트
+            UserChallenger(8, "임애플", "스위프트", 12, UserPart.IOS, UserChallengerRole.SUB_LEADER),
+            UserChallenger(9, "강아이", "엑스코드", 12, UserPart.IOS, UserChallengerRole.MEMBER),
+
+            // Android 파트
+            UserChallenger(10, "조안드", "코틀린", 12, UserPart.ANDROID, UserChallengerRole.PART_LEADER),
+            UserChallenger(11, "권젯팩", "컴포즈", 12, UserPart.ANDROID, UserChallengerRole.MEMBER),
+            UserChallenger(12, "성구글", "픽셀러버", 12, UserPart.ANDROID, UserChallengerRole.MEMBER),
+
+            // SpringBoot 파트
+            UserChallenger(13, "배엔드", "스프링", 12, UserPart.SPRING_BOOT, UserChallengerRole.PART_LEADER),
+            UserChallenger(14, "백자바", "제이피에이", 12, UserPart.SPRING_BOOT, UserChallengerRole.MEMBER),
+            UserChallenger(15, "유디비", "마이바티스", 12, UserPart.SPRING_BOOT, UserChallengerRole.MEMBER),
+
+            // Node.js 파트
+            UserChallenger(16, "노드정", "익스프레스", 12, UserPart.NODE_JS, UserChallengerRole.PART_LEADER),
+            UserChallenger(17, "신서버", "네스트", 12, UserPart.NODE_JS, UserChallengerRole.MEMBER)
+        )
+        updateState { copy(allChallengers = dummyList, filteredChallengers = dummyList) }
+    }
+
+    fun filterList(query: String) {
+        val filtered = uiState.value.allChallengers.filter {
+            it.name.contains(query) || it.nickname.contains(query)
+        }
+        updateState { copy(filteredChallengers = filtered) }
+    }
+
+    fun navigateToDetail(id: Int) {
+        emitEvent(UserChallengerEvent.NavigateToDetail(id))
+    }
+}
+
+data class UserChallengerUiState(
+    val allChallengers: List<UserChallenger> = emptyList(),
+    val filteredChallengers: List<UserChallenger> = emptyList()
+) : UiState
+
+sealed class UserChallengerEvent : UiEvent {
+    data class NavigateToDetail(val id: Int) : UserChallengerEvent()
+}
