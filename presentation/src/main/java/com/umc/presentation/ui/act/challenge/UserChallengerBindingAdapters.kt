a5062819d6952624291407a1668bc26770cda885
yangjiae12 <yangjiae12@gmail.com>
2026-01-22 19:52:29 +0900

    [refactor/#35] - UButton 테두리 두께 설정을 XML로 이동 및 바인딩 로직 단순화

--- presentation/src/main/java/com/umc/presentation/ui/act/challenge/UserChallengerBindingAdapters.kt
+++ presentation/src/main/java/com/umc/presentation/ui/act/challenge/UserChallengerBindingAdapters.kt
@@ -6,7 +6,6 @@ import androidx.databinding.BindingAdapter
 import com.umc.domain.model.enums.UserChallengerRole
 import com.umc.presentation.R
 import com.umc.presentation.component.UButton
-import com.umc.presentation.extension.px
 
 object UserChallengerBindingAdapters {
 
@@ -21,18 +20,17 @@ object UserChallengerBindingAdapters {
         view.visibility = View.VISIBLE
         view.setText(role.label)
 
-        // 색상 정의
+        // 역할별 색상 리소스 정의
         val (bgColorRes, textColorRes, borderColorRes) = when (role) {
-            UserChallengerRole.LEADER -> Triple(R.color.warning100, R.color.warning700, R.color.warning700)
-            UserChallengerRole.SUB_LEADER -> Triple(R.color.accent100, R.color.accent700, R.color.accent700)
-            UserChallengerRole.PART_LEADER -> Triple(R.color.success100, R.color.success700, R.color.success700)
+            UserChallengerRole.LEADER -> Triple(R.color.warning100, R.color.warning700, R.color.warning300)
+            UserChallengerRole.SUB_LEADER -> Triple(R.color.accent100, R.color.accent700, R.color.accent300)
+            UserChallengerRole.PART_LEADER -> Triple(R.color.success100, R.color.success700, R.color.success300)
             else -> Triple(R.color.neutral100, R.color.neutral700, R.color.neutral200)
         }
 
-        // 색상 적용
+        // 배경색, 텍스트색, 테두리색 적용
         view.setUBackgroundColor(ContextCompat.getColor(view.context, bgColorRes))
         view.setTextColor(ContextCompat.getColor(view.context, textColorRes))
         view.strokeColor = ContextCompat.getColor(view.context, borderColorRes)
-        view.strokeWidth = 1.px
     }
 }
