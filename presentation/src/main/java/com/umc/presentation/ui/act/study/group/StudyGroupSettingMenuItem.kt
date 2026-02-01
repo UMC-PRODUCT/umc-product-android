96caf8610005f20caf459b4dfe6928d5ca0cdbe2
Administrator <kdy99ky@gmail.com>
2026-02-01 21:48:58 +0900

    [feat] 설정 메뉴 구현

---
+++ presentation/src/main/java/com/umc/presentation/ui/act/study/group/StudyGroupSettingMenuItem.kt
@@ -0,0 +1,16 @@
+package com.umc.presentation.ui.act.study.group
+
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import com.umc.presentation.R
+
+enum class StudyGroupSettingAction { EDIT, DELETE }
+
+data class StudyGroupSettingMenuItem(
+    val action: StudyGroupSettingAction,
+    val title: String,
+    @DrawableRes val iconRes: Int,
+    @ColorRes val titleColorRes: Int = R.color.neutral800,
+    @ColorRes val iconTintRes: Int = R.color.neutral600,
+    @ColorRes val arrowTintRes: Int = R.color.neutral400,
+)
