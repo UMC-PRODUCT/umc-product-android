7fe006d372c4814710085aab950a8d954c1b7808
Park-yu-su <pys01022342867@gmail.com>
2026-02-01 16:46:57 +0900

    [feat/#38] - 게시글 검색 오류 수정

--- presentation/src/main/java/com/umc/presentation/ui/community/search/PostSearchFragment.kt
+++ presentation/src/main/java/com/umc/presentation/ui/community/search/PostSearchFragment.kt
@@ -7,25 +7,89 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.umc.domain.model.enums.SearchMode
+import com.umc.domain.model.home.ParticipantItem
+import com.umc.domain.model.mypage.ContentItem
 import com.umc.presentation.R
 import com.umc.presentation.base.BaseFragment
 import com.umc.presentation.databinding.FragmentPostSearchBinding
+import com.umc.presentation.ui.community.CommunityFragmentDirections
+import com.umc.presentation.ui.community.adapter.RecentSearchAdapter
+import com.umc.presentation.ui.community.adapter.RecentSearchDelegate
 import com.umc.presentation.ui.community.detail.PostDetailFragmentEvent
 import com.umc.presentation.ui.community.detail.PostDetailViewModel
+import com.umc.presentation.ui.home.PlanAddFragmentEvent
+import com.umc.presentation.ui.mypage.adapter.ContentAdapter
+import com.umc.presentation.ui.mypage.adapter.ContentItemDelegate
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class PostSearchFragment : BaseFragment<FragmentPostSearchBinding, PostSearchFragmentUiState, PostSearchFragmentEvent, PostSearchViewModel>(
     FragmentPostSearchBinding::inflate
-) {
+), ContentItemDelegate, RecentSearchDelegate {
     override val viewModel: PostSearchViewModel by viewModels()
 
+    // 검색 결과용 recycler 어댑터
+    private lateinit var searchContentAdapter : ContentAdapter
+    // 최근 검색 기록용 recycler 어댑터
+    private lateinit var recentSearchAdapter : RecentSearchAdapter
+    
+    // 최근 기록 검색(누를 시) 로직
+    override fun onRecentSearchClicked(keyword: String) {
+        viewModel.selectRecentSearch(keyword)
+    }
+    
+    // 최근 기록 X 버튼 누를 시 로직
+    override fun onDeleteClicked(keyword: String) {
+        viewModel.deleteRecentSearch(keyword)
+    }
+    
+    // 검색 결과 item 터치 시 이동 로직
+    override fun onItemClicked(item: ContentItem) {
+        val action = PostSearchFragmentDirections.actionPostSearchToPostDetail()
+        findNavController().navigate(action)
+    }
+
+    
+    
     override fun initView() {
         binding.apply {
             vm = viewModel
             lifecycleOwner = viewLifecycleOwner
         }
+        
+        // 텍스트 필드 정의
+        binding.searchTextfield.apply {
+            setOnTextChangedListener { text ->
+                //바뀔때마다 비교
+                /**여기서 아이콘 + ViewMode 바꾸기**/
+                if (text.isNotBlank()) {
+                    // 글자가 입력될 때마다 실시간으로 검색 내용 변경
+                    viewModel.onQueryChanged(text)
+                    viewModel.onChangeViewMode(SearchMode.TYPING)
+
+                } else {
+                    /**여기도 바꾸기**/
+                    viewModel.onQueryChanged(text)
+                    viewModel.onChangeViewMode(SearchMode.EMPTY)
+                }
+            }
+        }
+
+        //어댑터 정의
+        searchContentAdapter = ContentAdapter(this)
+        binding.searchRcvResult.apply{
+            adapter = searchContentAdapter
+        }
+
+        recentSearchAdapter = RecentSearchAdapter(this)
+        binding.searchRcvRecent.apply{
+            adapter = recentSearchAdapter
+        }
+
+
 
 
     }
@@ -36,7 +100,12 @@ class PostSearchFragment : BaseFragment<FragmentPostSearchBinding, PostSearchFra
         repeatOnStarted(viewLifecycleOwner){
             launch {
                 viewModel.uiState.collect { state ->
+                    //UISTATE의 모드 최신화
+                    handleSearchMode(state.mode)
 
+                    //어댑터
+                    searchContentAdapter.submitList(state.searchResults)
+                    recentSearchAdapter.submitList(state.recentSearches)
                 }
             }
 
@@ -53,11 +122,43 @@ class PostSearchFragment : BaseFragment<FragmentPostSearchBinding, PostSearchFra
         super.handleEvent(event)
 
         when(event){
+            
+            //검색 결과를 눌렀을 때 메소드
             is PostSearchFragmentEvent.ShowSearchResult -> {
-                Toast.makeText(requireContext(), "검색 결과", Toast.LENGTH_SHORT).show()
+            /**TODO 서버에서 검색 결과를 받아와서 uistate에 넣어주기**/
+                binding.searchTextfield.setText(event.query)
+                Toast.makeText(requireContext(), "${event.query} 검색 결과", Toast.LENGTH_SHORT).show()
             }
+
+            is PostSearchFragmentEvent.MoveBackPressedEvent -> {
+                findNavController().popBackStack()
+            }
+
             else -> {}
         }
-
     }
+
+    //타입에 따라 다르게 보이게 하기
+    private fun handleSearchMode(mode: SearchMode) {
+        binding.apply {
+            when (mode) {
+                SearchMode.EMPTY -> {
+                    searchLayoutRecent.visibility = View.VISIBLE
+                    searchLayoutResult.visibility = View.GONE
+                }
+
+                SearchMode.TYPING -> {
+                    searchLayoutRecent.visibility = View.GONE
+                    searchLayoutResult.visibility = View.GONE
+                }
+
+                SearchMode.RESULT -> {
+                    searchLayoutRecent.visibility = View.GONE
+                    searchLayoutResult.visibility = View.VISIBLE
+                }
+            }
+        }
+    }
+
+
 }
--- presentation/src/main/java/com/umc/presentation/ui/community/search/PostSearchViewModel.kt
+++ presentation/src/main/java/com/umc/presentation/ui/community/search/PostSearchViewModel.kt
@@ -2,9 +2,16 @@ package com.umc.presentation.ui.community.search
 
 import android.view.inputmethod.EditorInfo
 import android.widget.Toast
+import com.umc.domain.model.enums.CommunityCategoryType
+import com.umc.domain.model.enums.ContentType
+import com.umc.domain.model.enums.RecruitType
+import com.umc.domain.model.enums.SearchMode
+import com.umc.domain.model.enums.UserPart
+import com.umc.domain.model.mypage.ContentItem
 import com.umc.presentation.base.BaseViewModel
 import com.umc.presentation.base.UiEvent
 import com.umc.presentation.base.UiState
+import com.umc.presentation.ui.home.PlanDetailFragmentEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -16,27 +23,143 @@ constructor() : BaseViewModel<PostSearchFragmentUiState, PostSearchFragmentEvent
 ) {
 
 
-    // 검색 창 엔터 관련 메서드
+    // 검색 창 엔터 관련 메서드 (검색 결과 시 처리)
     fun onImeAction(actionId: Int, text: String): Boolean {
-        if (actionId == EditorInfo.IME_ACTION_DONE) {
-            emitEvent(PostSearchFragmentEvent.ShowSearchResult)
+        if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+            updateState {
+                copy(
+                    mode = SearchMode.RESULT,
+                )
+            }
+            /**TODO 서버에서 처리한 결과가 들어가기**/
+            emitEvent(PostSearchFragmentEvent.ShowSearchResult(uiState.value.query))
+
             return true
         }
         return false
     }
 
+    // 실시간 텍스트 변경 처리
+    fun onQueryChanged(query: String) {
+        updateState {
+            copy(query = query)
+        }
+    }
+
+    // 최근 검색어 선택 시 쿼리 변경 및 결과 모드 처리
+    fun selectRecentSearch(keyword: String) {
+        updateState {
+            copy(
+                query = keyword,
+                mode = SearchMode.RESULT
+            )
+        }
+        // 검색 로직 실행 (엔터 친 것과 동일)
+        emitEvent(PostSearchFragmentEvent.ShowSearchResult(uiState.value.query))
+    }
+
+    // 최근 검색어 리스트에서 X 클릭 시 -> 해당 키워드 제거
+    fun deleteRecentSearch(keyword: String) {
+        updateState {
+            val updatedList = recentSearches.filter { it != keyword }
+            copy(recentSearches = updatedList)
+        }
+        /**TODO 서버 로직**/
+    }
+
+    // 최근 검색어 키워드 전체 삭제
+    fun deleteAllRecentSearch(){
+        updateState {
+            copy(recentSearches = listOf())
+        }
+        /**TODO 서버 로직**/
+    }
+
+    //뷰모드 변경하기
+    fun onChangeViewMode(mode: SearchMode){
+        updateState {
+            copy(mode = mode)
+        }
+    }
+
+
+    fun onClickBackPressed(){
+        emitEvent(PostSearchFragmentEvent.MoveBackPressedEvent)
+    }
 
 }
 
 
 data class PostSearchFragmentUiState(
 
-    val dummy: Boolean = false,
+    //현재 검색창에 입력한 내용
+    val query : String = "",
+    //뷰모드
+    val mode : SearchMode = SearchMode.EMPTY,
+    //최근 검색 내용
+    val recentSearches: List<String> = listOf("중앙", "중앙 해커톤"),
+    //검색 결과
+    val searchResults: List<ContentItem> = listOf(
+        ContentItem(
+            category = CommunityCategoryType.QUESTION,
+            region = "서울",
+            contentType = ContentType.ALL,
+            recruitType = RecruitType.END,
+            title = "이거는 제목이에요!!!!!!!!!!!!!!!!",
+            username = "어헛차",
+            writeTime = "방금 전",
+            likes = 0,
+            comments = 1,
+            content = "이거는 본문 내용이에요!!!",
+            userPart = UserPart.ANDROID,
+        ),
+        ContentItem(
+            category = CommunityCategoryType.HOBBY,
+            region = "인천",
+            contentType = ContentType.ALL,
+            recruitType = RecruitType.RECRUIT,
+            title = "이거는 제목이에요!!!!!!!!!!!!!!!!",
+            username = "어헛차2호",
+            writeTime = "1시간 전",
+            likes = 2,
+            comments = 2,
+            content = "이거는 본문 내용이에요!!!",
+            userPart = UserPart.WEB,
+        ),
+        ContentItem(
+            category = CommunityCategoryType.SUGGESTION,
+            region = "인천",
+            contentType = ContentType.QUESTION,
+            recruitType = RecruitType.RECRUIT,
+            title = "이거는 제목이에요!!!!!!!!!!!!!!!!",
+            username = "사람",
+            writeTime = "2016.01.19",
+            likes = 200,
+            comments = 123,
+            content = "이거는 본문 내용이에요!!!",
+            userPart = UserPart.IOS,
+        ),
+        ContentItem(
+            category = CommunityCategoryType.LIGHTNING,
+            region = "인천",
+            contentType = ContentType.QUESTION,
+            recruitType = RecruitType.END,
+            title = "밥먹고개발하고쉬고개발하고게임하고개발하고자고개발하고나는개발이너무너무너무좋아헤헤헤헤헤헿",
+            username = "사람",
+            writeTime = "2016.01.19",
+            likes = 10,
+            comments = 1,
+            content = "본문내용을늘려야하는데무슨내용을적어야잘적었다는소문이날까?내용을늘리기위해서는아무내용이나넣어야겠다.이쯤되면길어지겠지?",
+            userPart = UserPart.SPRING_BOOT,
+        ),
+    ),
 
 
     ) : UiState
 
 sealed interface PostSearchFragmentEvent : UiEvent {
-    object ShowSearchResult : PostSearchFragmentEvent
+    data class ShowSearchResult(val query : String) : PostSearchFragmentEvent
+
+    object MoveBackPressedEvent : PostSearchFragmentEvent
 
 }
