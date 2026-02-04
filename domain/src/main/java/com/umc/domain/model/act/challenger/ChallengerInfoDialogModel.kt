81dd0dd8b803051a3479546658f19085abd8479e
yangjiae12 <yangjiae12@gmail.com>
2026-02-04 15:37:36 +0900

    [feat/#57] - 챌린저 정보 모델 수정

--- domain/src/main/java/com/umc/domain/model/act/challenger/ChallengerInfoDialogModel.kt
+++ domain/src/main/java/com/umc/domain/model/act/challenger/ChallengerInfoDialogModel.kt
@@ -5,19 +5,20 @@ import com.umc.domain.model.enums.CheckHistoryStatus
 /**
  * 다이얼로그의 출석/활동 기록 리스트 아이템 모델
  */
-data class SimpleHistoryItem(
-    val title: String,
-    val status: CheckHistoryStatus
+data class ChallengerInfoHistory(
+    val title: String = "",
+    val status: CheckHistoryStatus = CheckHistoryStatus.ABSENT
 )
 
 /**
  * 챌린저 상세 정보 다이얼로그 전체 데이터 모델
  */
 data class ChallengerInfoDialogModel(
-    val name: String,
-    val university: String,
-    val part: String,
-    val generation: String = "12기",
+    val name: String = "알수없음",
+    val university: String = "알수없음",
+    val part: String = "알수없음",
+    val generation: Int = 0,
+    val profileImageUrl: String = "",
     val warningCount: Double = 0.0,
-    val history: List<SimpleHistoryItem> = emptyList()
+    val history: List<ChallengerInfoHistory> = emptyList()
 )
