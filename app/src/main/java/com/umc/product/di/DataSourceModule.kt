3bb81954545955e376e2e78e2d63f5295cade350
yangjiae12 <yangjiae12@gmail.com>
2026-02-04 15:40:34 +0900

    [feat/#57] - 챌린저 관련 의존성 주입(DI) 설정

--- app/src/main/java/com/umc/product/di/DataSourceModule.kt
+++ app/src/main/java/com/umc/product/di/DataSourceModule.kt
@@ -1,7 +1,9 @@
 package com.umc.product.di
 
 import com.umc.data.dataSource.AuthRemoteDataSource
+import com.umc.data.dataSource.ChallengerRemoteDataSource
 import com.umc.data.dataSource.remote.AuthRemoteDataSourceImpl
+import com.umc.data.dataSource.remote.ChallengerRemoteDataSourceImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -14,4 +16,8 @@ abstract class DataSourceModule {
     @Singleton
     @Binds
     abstract fun providesAuthDataSource(dataSourceImpl: AuthRemoteDataSourceImpl): AuthRemoteDataSource
+
+    @Singleton
+    @Binds
+    abstract fun bindsChallengerRemoteDataSource(dataSourceImpl: ChallengerRemoteDataSourceImpl): ChallengerRemoteDataSource
 }
--- app/src/main/java/com/umc/product/di/RepositoryModule.kt
+++ app/src/main/java/com/umc/product/di/RepositoryModule.kt
@@ -1,7 +1,9 @@
 package com.umc.product.di
 
 import com.umc.data.repository.AuthRepositoryImpl
+import com.umc.data.repository.ChallengerRepositoryImpl
 import com.umc.domain.repository.AuthRepository
+import com.umc.domain.repository.ChallengerRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -14,4 +16,8 @@ abstract class RepositoryModule {
     @Singleton
     @Binds
     abstract fun providesAuthRepository(repositoryImpl: AuthRepositoryImpl): AuthRepository
+
+    @Singleton
+    @Binds
+    abstract fun bindsChallengerRepository(repositoryImpl: ChallengerRepositoryImpl): ChallengerRepository
 }
