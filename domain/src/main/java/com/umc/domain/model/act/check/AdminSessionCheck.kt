ee78f52da0a464d41f2307e2bc6553bcffa520e1
yangjiae12 <yangjiae12@gmail.com>
2026-02-07 20:03:39 +0900

    [feat/#69] - 관리자 일정 조회 도메인 모델 및 유즈케이스 구현

--- domain/src/main/java/com/umc/domain/model/act/check/AdminSessionCheck.kt
+++ domain/src/main/java/com/umc/domain/model/act/check/AdminSessionCheck.kt
@@ -1,14 +1,15 @@
 package com.umc.domain.model.act.check
 
-import com.umc.domain.model.enums.AdminSessionStatus
-
 data class AdminSessionCheck(
     val id: Int,
     val title: String,
     val date: String,
     val startTime: String,
     val endTime: String,
-    val adminStatus: AdminSessionStatus,
-    val stats: AdminCheckStats,
+    val status: String,
+    val attendanceRate: Int,        // 출석률
+    val totalChallengers: Int,      // 전체 인원
+    val attendedChallengers: Int,   // 출석 완료 인원
+    val pendingCount: Int,          // 승인 대기 인원
     val pendingUsers: List<AdminPendingUser>
 )
