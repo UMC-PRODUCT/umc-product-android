66f8d4d82c7ca0a81903a3b1666cf0c2b581f645
seok <rudtjr1206@naver.com>
2026-02-09 10:44:20 +0900

    [feat/#70] - dataSource 추가

--- app/src/main/java/com/umc/product/di/DataSourceModule.kt
+++ app/src/main/java/com/umc/product/di/DataSourceModule.kt
@@ -1,8 +1,10 @@
 package com.umc.product.di
 
 import com.umc.data.dataSource.AuthRemoteDataSource
+import com.umc.data.dataSource.OrganizationDataSource
 import com.umc.data.dataSource.remote.challenger.ChallengerRemoteDataSource
 import com.umc.data.dataSource.remote.AuthRemoteDataSourceImpl
+import com.umc.data.dataSource.remote.OrganizationRemoteDataSourceImpl
 import com.umc.data.dataSource.remote.attendance.AttendanceRemoteDataSource
 import com.umc.data.dataSource.remote.attendance.AttendanceRemoteDataSourceImpl
 import com.umc.data.dataSource.remote.kakao.KakaoRemoteDataSource
@@ -25,11 +27,14 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class DataSourceModule {
+
     @Singleton
     @Binds
     abstract fun providesAuthDataSource(dataSourceImpl: AuthRemoteDataSourceImpl): AuthRemoteDataSource
 
-
+    @Singleton
+    @Binds
+    abstract fun providesOrganizationDataSource(dataSourceImpl: OrganizationRemoteDataSourceImpl): OrganizationDataSource
 
     @Singleton
     @Binds
