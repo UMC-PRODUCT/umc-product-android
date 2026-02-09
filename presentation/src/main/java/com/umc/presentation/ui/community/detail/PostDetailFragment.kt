bdf2be9fe47d49f8921041e2d9f10921da57f6fd
Park-yu-su <pys01022342867@gmail.com>
2026-02-09 16:13:10 +0900

    [feat] - 댓글 삭제 UI 연동 완료

--- presentation/src/main/java/com/umc/presentation/ui/community/detail/PostDetailFragment.kt
+++ presentation/src/main/java/com/umc/presentation/ui/community/detail/PostDetailFragment.kt
@@ -58,8 +58,8 @@ class PostDetailFragment : BaseFragment<FragmentPostDetailBinding, PostDetailFra
             elevation = 20f
         }
 
-        // 내 댓글인지 여부에 따른 가시성 조절 (임시 로직)
-        val isMyComment = item.challengerName == "새 유저"
+        // TODO : chllengerId 체크 필요!
+        val isMyComment = item.challengerId == viewModel.uiState.value.myId
         menuBinding.layoutMenuReport.visibility = if (isMyComment) View.GONE else View.VISIBLE
         menuBinding.layoutMenuDelete.visibility = if (isMyComment) View.VISIBLE else View.GONE
 
