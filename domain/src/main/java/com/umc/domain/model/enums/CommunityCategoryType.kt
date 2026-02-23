b3132aa0341386811a595e8025a31377c72b0ea1
Park-yu-su <pys01022342867@gmail.com>
2026-02-23 15:01:57 +0900

    [refactor] 커뮤니티 게시글 타입 수정

--- domain/src/main/java/com/umc/domain/model/enums/CommunityCategoryType.kt
+++ domain/src/main/java/com/umc/domain/model/enums/CommunityCategoryType.kt
@@ -3,8 +3,6 @@ package com.umc.domain.model.enums
 //커뮤니티에서의 카테고리 타입
 enum class CommunityCategoryType(val label: String) {
     LIGHTNING("번개"),
-    HABIT("취미"),
     QUESTION("질문"),
-    INFORMATION("정보"),
     FREE("자유"),
 }
