c148a5cb0b7a33122d953ab43bc6f57e7e8814a6
Administrator <kdy99ky@gmail.com>
2026-03-12 22:27:56 +0900

    [feat] 스터디 그룹 권한에 따른 토스트 추가

--- presentation/src/main/java/com/umc/presentation/ui/act/study/group/model/AdminStudyGroupEvent.kt
+++ presentation/src/main/java/com/umc/presentation/ui/act/study/group/model/AdminStudyGroupEvent.kt
@@ -3,7 +3,7 @@ package com.umc.presentation.ui.act.study.group.model
 import com.umc.presentation.base.UiEvent
 
 sealed interface AdminStudyGroupEvent : UiEvent {
-
+    data object ClickCreateGroup : AdminStudyGroupEvent
+    data class ClickEditMembers(val groupId: Long) : AdminStudyGroupEvent
     data class ShowToast(val message: String) : AdminStudyGroupEvent
-
 }
