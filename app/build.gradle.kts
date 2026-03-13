823ac0725209c3c22832ce101ba118020ae6ec12
seok <rudtjr1206@naver.com>
2026-03-14 01:52:31 +0900

    [Chore/#159] - 버전 업

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.umc.product"
         minSdk = 24
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.1.2"
+        versionCode = 12
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
