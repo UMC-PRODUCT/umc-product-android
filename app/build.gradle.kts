bf7f286f5b9faec697931ea5e41268837571183e
seok <rudtjr1206@naver.com>
2026-03-13 11:46:27 +0900

    [Fix/#159] - 회원탈퇴 기능 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.umc.product"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
