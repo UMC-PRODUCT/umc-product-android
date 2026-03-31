015083bd8c6a388eab021fb751f15e9d2ee902d4
seok <rudtjr1206@naver.com>
2026-03-31 14:49:54 +0900

    [feat-#205] - UText 추가

---
+++ presentation/src/main/java/com/umc/presentation/component/UText.kt
@@ -0,0 +1,89 @@
+package com.umc.presentation.component
+
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Density
+
+@Composable
+fun UText(
+    text: String,
+    modifier: Modifier = Modifier,
+    color: Color = Color.Unspecified,
+    textDecoration: TextDecoration? = null,
+    textAlign: TextAlign? = null,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    minLines: Int = 1,
+    onTextLayout: ((TextLayoutResult) -> Unit)? = null,
+    style: TextStyle = LocalTextStyle.current
+) {
+    val currentDensity = LocalDensity.current
+    val fixedFontScaleDensity = remember(currentDensity) {
+        Density(density = currentDensity.density, fontScale = 1f)
+    }
+
+    CompositionLocalProvider(LocalDensity provides fixedFontScaleDensity) {
+        Text(
+            text = text,
+            style = style,
+            color = color,
+            modifier = modifier,
+            textDecoration = textDecoration,
+            textAlign = textAlign,
+            overflow = overflow,
+            softWrap = softWrap,
+            maxLines = maxLines,
+            minLines = minLines,
+            onTextLayout = onTextLayout ?: {}
+        )
+    }
+}
+
+@Composable
+fun HuggText(
+    text: AnnotatedString,
+    modifier: Modifier = Modifier,
+    color: Color = Color.Unspecified,
+    textDecoration: TextDecoration? = null,
+    textAlign: TextAlign? = null,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    minLines: Int = 1,
+    onTextLayout: ((TextLayoutResult) -> Unit)? = null,
+    style: TextStyle = LocalTextStyle.current
+) {
+    val currentDensity = LocalDensity.current
+    val fixedFontScaleDensity = remember(currentDensity) {
+        Density(density = currentDensity.density, fontScale = 1f)
+    }
+
+    CompositionLocalProvider(LocalDensity provides fixedFontScaleDensity) {
+        Text(
+            text = text,
+            style = style,
+            color = color,
+            modifier = modifier,
+            textDecoration = textDecoration,
+            textAlign = textAlign,
+            overflow = overflow,
+            softWrap = softWrap,
+            maxLines = maxLines,
+            minLines = minLines,
+            onTextLayout = onTextLayout ?: {}
+        )
+    }
+}
