858d33f70359a655804070708f75de01e33812c7
seok <rudtjr1206@naver.com>
2026-07-20 14:18:29 +0900

    [Chore-250] 공지 작성 문자열·아이콘 리소스 및 coil 의존성 추가

--- presentation/notice/build.gradle.kts
+++ presentation/notice/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.coil.compose)
     implementation(libs.hilt.android)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.lifecycle.runtime.compose)
