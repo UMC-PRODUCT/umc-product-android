36e4004b5144ddc440d4a132ff97e051ab2e3d10
Administrator <kdy99ky@gmail.com>
2026-08-01 03:05:25 +0900

    [feat-#259] 커뮤니티 검색 기능 구현

---
+++ presentation/community/src/main/java/com/umc/presentation/community/search/CommunitySearchAction.kt
@@ -0,0 +1,30 @@
+package com.umc.presentation.community.search
+
+sealed interface CommunitySearchAction {
+
+    data class OnQueryChanged(
+        val query: String,
+    ) : CommunitySearchAction
+
+    data object OnSearchClick : CommunitySearchAction
+
+    data object OnCancelClick : CommunitySearchAction
+
+    data object OnBackClick : CommunitySearchAction
+
+    data object OnClearQueryClick : CommunitySearchAction
+
+    data object OnClearAllRecentSearchesClick : CommunitySearchAction
+
+    data class OnRecentSearchClick(
+        val query: String,
+    ) : CommunitySearchAction
+
+    data class OnDeleteRecentSearchClick(
+        val query: String,
+    ) : CommunitySearchAction
+
+    data class OnThreadClick(
+        val threadId: Long,
+    ) : CommunitySearchAction
+}
---
+++ presentation/community/src/main/java/com/umc/presentation/community/search/CommunitySearchState.kt
@@ -0,0 +1,17 @@
+package com.umc.presentation.community.search
+
+import com.umc.presentation.community.model.CommunityThreadUiModel
+
+data class CommunitySearchState(
+    val query: String = "",
+    val recentSearches: List<String> = emptyList(),
+    val searchResults: List<CommunityThreadUiModel> = emptyList(),
+    val hasSearched: Boolean = false,
+    val isLoading: Boolean = false,
+) {
+    val isInitial: Boolean
+        get() = !hasSearched
+
+    val isResultEmpty: Boolean
+        get() = hasSearched && searchResults.isEmpty()
+}
