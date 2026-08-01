c1b2607122f13a12be205f946fd37843d1f15f32
Administrator <kdy99ky@gmail.com>
2026-08-02 00:33:21 +0900

    [feat-#259] 스레드 생성 및 스레드 수정 기능 추가

---
+++ presentation/community/src/main/java/com/umc/presentation/community/edit/CommunityEditState.kt
@@ -0,0 +1,35 @@
+package com.umc.presentation.community.edit
+
+import com.umc.presentation.community.model.CommunityAiState
+import com.umc.presentation.community.model.CommunityCategory
+import com.umc.presentation.community.model.CommunityChallengerUiModel
+
+data class CommunityEditState(
+    val threadId: Long = 0L,
+
+    val title: String = "",
+    val description: String = "",
+
+    val selectedChallengers: List<CommunityChallengerUiModel> = emptyList(),
+    val maxChallengerCount: Int = 8,
+
+    val aiState: CommunityAiState = CommunityAiState.SUCCESS,
+    val classifiedCategory: CommunityCategory? = CommunityCategory.STUDY,
+
+    val isSaving: Boolean = false,
+    val showChallengerBottomSheet: Boolean = false,
+    val showDeleteDialog: Boolean = false,
+) {
+    val selectedChallengerCountText: String
+        get() = "${selectedChallengers.size} / $maxChallengerCount"
+
+    val isSaveEnabled: Boolean
+        get() = title.isNotBlank() &&
+                description.isNotBlank() &&
+                !isSaving
+
+    val canRequestClassification: Boolean
+        get() = title.isNotBlank() &&
+                description.isNotBlank() &&
+                aiState != CommunityAiState.LOADING
+}
