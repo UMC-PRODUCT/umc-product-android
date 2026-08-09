4d4f02e8815b0d3d018a8bb1b98e84fa32efc186
Administrator <kdy99ky@gmail.com>
2026-08-09 19:57:36 +0900

    [feat-#252] 기존코드 수정

--- presentation/src/main/java/com/umc/presentation/MainNavHost.kt
+++ presentation/src/main/java/com/umc/presentation/MainNavHost.kt
@@ -59,7 +59,7 @@ fun MainNavHost(
 
 
         startDestination = if (BuildConfig.DEBUG) {
-            MainDestination.Act
+            MainDestination.Login
         } else {
             MainDestination.Home
         },
