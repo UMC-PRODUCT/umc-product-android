646203466f0736d467f18affb2768c0d572a73fa
seok <rudtjr1206@naver.com>
2026-08-18 23:22:51 +0900

    [Fix-266] 커뮤니티 스레드 생성 시 본인을 참여자로 추가하지 못하도록 차단

--- presentation/component/src/main/java/com/umc/component/theme/AppStrings.kt
+++ presentation/component/src/main/java/com/umc/component/theme/AppStrings.kt
@@ -487,6 +487,7 @@ object AppStrings {
     const val REWARD_ETC_TITLE = "기타 상벌점 부여하기"
     const val REWARD_ETC_CONTENT = "기타 상벌점 사유와 점수를 입력해 주세요."
     const val REWARD = "상점"
+    const val COMMUNITY_CREATE_SELF_NOT_ALLOWED = "본인은 참여자로 추가할 수 없어요."
     const val PUNISH = "벌점"
     const val HAB = "총합"
     const val REWARD_ETC_REASON = "사유"
