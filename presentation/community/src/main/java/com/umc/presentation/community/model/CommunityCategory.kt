3e778ea89a8b52f764226658040c86af8a456de0
Administrator <kdy99ky@gmail.com>
2026-08-18 21:55:16 +0900

    [docs-#270] 커뮤니티 모델 주석추가

--- presentation/community/src/main/java/com/umc/presentation/community/model/CommunityCategory.kt
+++ presentation/community/src/main/java/com/umc/presentation/community/model/CommunityCategory.kt
@@ -1,5 +1,11 @@
 package com.umc.presentation.community.model
 
+/**
+ * 커뮤니티 스레드에서 사용하는 카테고리
+ *
+ * 스레드 분류 및 목록 필터링에 사용되며,
+ * 각 카테고리가 화면에 표시할 한글 라벨을 관리합니다.
+ */
 enum class CommunityCategory(
     val label: String,
 ) {
