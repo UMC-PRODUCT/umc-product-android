78a6e8f44eff599f3a74adb04549c0eb26309f1f
Administrator <kdy99ky@gmail.com>
2026-08-18 21:55:16 +0900

    [docs-#270] 커뮤니티 모델 주석추가

--- presentation/community/src/main/java/com/umc/presentation/community/model/CommunityAiState.kt
+++ presentation/community/src/main/java/com/umc/presentation/community/model/CommunityAiState.kt
@@ -1,5 +1,10 @@
 package com.umc.presentation.community.model
 
+/**
+ * 커뮤니티 스레드의 AI 카테고리 분류 상태
+ *
+ * 분류 안내, 로딩, 성공, 재분류 필요, 실패 상태를 관리합니다.
+ */
 enum class CommunityAiState {
     GUIDE,
     LOADING,
--- presentation/community/src/main/java/com/umc/presentation/community/model/CommunityChallengerUiModel.kt
+++ presentation/community/src/main/java/com/umc/presentation/community/model/CommunityChallengerUiModel.kt
@@ -1,5 +1,11 @@
 package com.umc.presentation.community.model
 
+/**
+ * 커뮤니티에서 챌린저 정보를 화면에 표시하기 위한 UI 모델
+ *
+ * 챌린저의 기본 정보와 프로필 이미지를 관리하며,
+ * 이름과 닉네임을 조합한 표시용 이름을 제공합니다.
+ */
 data class CommunityChallengerUiModel(
     val memberId: Long,
     val name: String,
